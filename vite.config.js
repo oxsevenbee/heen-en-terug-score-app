22982d710fb28d16b6f9cc2be458b2016f6e8dc7
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2024-09-23 11:58:52 +0200

    Dev on iOS device

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import rust from "@wasm-tool/rollup-plugin-rust";
 
+const host = process.env.TAURI_DEV_HOST;
 
 let rustPluginInstance = rust({
   verbose: true,
@@ -16,5 +17,11 @@ rustPluginInstance["handleHotUpdate"] = (ctx) => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    host: host || false,
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [svelte(), rustPluginInstance],
 })
